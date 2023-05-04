4b78c1a48393acc8ff48df8199ce0294cb51b75b
threadsv1 <majdimed3@gmail.com>
2023-05-04 22:44:42 +0200

    unkwerr

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGO_H_
+#define _SEARCH_ALGO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _advanced_binary(int *original, int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGO_H_ */
