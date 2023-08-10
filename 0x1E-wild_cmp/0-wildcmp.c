616b9d3cf82b074db68ca2d8fb52b7d488025791
majdmednini <majdimed3@gmail.com>
2023-08-11 00:05:35 +0100

    betty

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,28 +1,37 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- * @s1: string to compare to
- * @s2: string with wild character
+ * wildcmp - compares two strings
+ * @s1: string to compare against
+ * @s2: string to compare
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 1 if identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 == '*')
+	if (*s1 == *s2 && *s1 != '\0')
 	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
-
+	if (*s1 == *s2)
+	{
+		return (1);
+	}
+	if (*s2 == '*' && *(s2 + 1) != *s1 && *(s2 + 1) != '*')
+	{
+		return (wildcmp(s1 + 1, s2));
+	}
+	if (*s2 == '*' && *(s2 + 1) != *s1 && *(s2 + 1) == '*')
+	{
+		return (wildcmp(s1, s2 + 1));
+	}
+	if (*s2 == '*' && wildcmp(s1, s2 + 1) == 0)
+	{
+		return (wildcmp(s1 + 1, s2));
+	}
+	if (*s2 == '*' && wildcmp(s1, s2 + 1) == 1)
+	{
+		return (1);
+	}
 	return (0);
 }
--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -1,6 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
 
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* _HOLBERTON_H_ */
